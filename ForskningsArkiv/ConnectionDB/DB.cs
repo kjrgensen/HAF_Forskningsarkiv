7e8811dd6fed09e6d672ddb02478b6f1744579cf
herprit <herpritsingh12@gmail.com>
2015-07-08 18:13:10 +0200

    Oprettet forbindelse til DB, samt fået Printet dvs. dataud i listBox.

---
+++ ForskningsArkiv/ConnectionDB/DB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ForskningsArkiv.ConnectionDB
+{
+    
+    
+    public class DB 
+    {
+        private SqlConnection constring;
+        private SqlCommand cmd1;
+        private SqlDataAdapter da;
+    
+        private Form1 f1;
+
+        private SearchForm SF; 
+
+        public void connection()
+        {
+           constring = new SqlConnection(
+                "Data Source=HERPRIT\\SQLEXPRESS;Initial Catalog=Forskningsarkiv(Sagnlandet);Persist Security Info=True;User ID=sa;Password=titan");
+            constring.Open();
+
+            MessageBox.Show("connected");
+
+            da = new SqlDataAdapter("SELECT * From tblEmnetyper", constring);
+           
+            DataTable emnetyper= new DataTable();
+            da.Fill(emnetyper);
+           
+    
+          
+
+
+            //cmd1.Connection = constring;
+
+            //dt = new DataTable();
+            //da.Fill(dt);
+
+
+            //cmd1 = new SqlCommand("SELECT * From tblEmnetyper", constring);
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
--- ForskningsArkiv/SearchForm.cs
+++ ForskningsArkiv/SearchForm.cs
@@ -2,19 +2,26 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ForskningsArkiv.ConnectionDB;
 
 namespace ForskningsArkiv
 {
     public partial class SearchForm : Form
     {
+        private DB d;
+        
         public SearchForm()
         {
             InitializeComponent();
+            //DB d = new DB();
+            //d.connection();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -22,6 +29,52 @@ namespace ForskningsArkiv
             Form1 form1 = new Form1();
             form1.Show();
             this.Close();
+        }
+
+        public void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+         //d=new DB();
+         //   d.connection();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //d = new DB();
+            //d.connection();
+            //listBox1.Items.Add(d);
+
+            SqlConnection constring = new SqlConnection(
+               "Data Source=HERPRIT\\SQLEXPRESS;Initial Catalog=Forskningsarkiv(Sagnlandet);Persist Security Info=True;User ID=sa;Password=titan");
+            constring.Open();
+
+            MessageBox.Show("connected");
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * From tblEmnetyper", constring);
+
+            DataTable emnetyperDT = new DataTable();
+            da.Fill(emnetyperDT);
+
+
+            //viser emnetype og beskrivelse i listbox
+            foreach (DataRow row in emnetyperDT.Rows)
+            {
+                listBox1.Items.Add("EmneTyper: "+ row["emnetype"].ToString());
+                listBox1.Items.Add("Beskrivelse: "+ row["beskrivelse"].ToString());
+            }
+
+
+
+
+            //listBox1.DataSource = emnetyper;
+            //listBox1.DisplayMember = "emnetype";
+            //listBox1.DisplayMember = "beskrivelse";
+            //listBox1.ValueMember = "emneID";
+
+
         }
     }
 }
