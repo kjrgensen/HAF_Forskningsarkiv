8ccbb4b7729d1bdd03ec3e36aa2120520b6dfa14
Herprit Singh <herprit_singh@hotmail.com>
2015-09-03 20:10:59 +0200

    cssac

--- ForskningsArkiv/SearchForm.Designer.cs
+++ ForskningsArkiv/SearchForm.Designer.cs
@@ -46,6 +46,8 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 296);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
             this.dataGridView1.Size = new System.Drawing.Size(739, 184);
             this.dataGridView1.TabIndex = 6;
             // 
