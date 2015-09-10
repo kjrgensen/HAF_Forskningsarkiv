86632f6be750f1e0881c7eb3f3a88e1605273cc3
Herprit Singh <herprit_singh@hotmail.com>
2015-09-10 18:26:28 +0200

    ANDAND virker plus combobox filtering

---
+++ ForskningsArkiv/DatabaseHAF/SøgDB/MaterialetypeSøg.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ForskningsArkiv.ConnectionDB;
+
+namespace ForskningsArkiv.DatabaseHAF.SøgDB
+{
+   public class MaterialetypeSøg
+    {
+
+        public DBConnectionstring _db;
+
+
+        public void SøgSpecifiktMaterialetypeFriTeskt(SearchForm searchForm)
+        {
+
+            _db = new DBConnectionstring();
+            var constring = new SqlConnection(_db.DbConnectionString);
+
+            constring.Open();
+
+            var sqlDataAdapter1 =
+                new SqlDataAdapter(
+                    "Select tblMaterialetyper.materialetype, tblMaterialetyper.beskrivelse, tblSagsoplysninger.sagens_titel, tblSagsoplysninger.journalNr from tblSagsoplysninger, tblMaterialetyper" +
+                    " where materialetype='" + searchForm.MatriealetypeBox.SelectedItem +
+                    "' and tblSagsoplysninger.sagens_titel like'" + searchForm.textBox1Søg.Text + "%'"
+                    ,
+                    constring);
+
+            var datatable1 = new DataTable();
+            sqlDataAdapter1.Fill(datatable1);
+
+            if (datatable1.Rows.Count == 0)
+            {
+                MessageBox.Show("Ingen rapporter fundet");
+            }
+            else
+            {
+                MessageBox.Show("fundet =" + datatable1.Rows.Count);
+            }
+
+            searchForm.dataGridView1.DataSource = datatable1;
+            searchForm.Refresh();
+        }
+
+
+        public void FillMaterielTypeComboBox(SearchForm searchForm)
+       {
+          
+            _db = new DBConnectionstring();
+            var constring = new SqlConnection(_db.DbConnectionString);
+
+            constring.Open();
+
+            var SqlDataAdapterEm = new SqlDataAdapter("select * from tblMaterialeTyper order by materialetype", constring);
+
+            var datatableCmEm = new DataTable();
+
+            SqlDataAdapterEm.Fill(datatableCmEm);
+
+            for (var i = 0; i < datatableCmEm.Rows.Count; i++)
+            {
+                searchForm.MatriealetypeBox.Items.Add(datatableCmEm.Rows[i]["materialetype"]);
+            }
+            constring.Close();
+
+
+
+
+        }
+
+
+
+    }
+}
